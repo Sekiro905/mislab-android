24368e416f739744785c3a11433d7d9c6db9f902
lzl <1216191040@qq.com>
2022-08-15 16:21:47 +0800

    添加了首页图形的动画.

--- src/components/Product/index.jsx
+++ src/components/Product/index.jsx
@@ -3,13 +3,15 @@ import './index.css'
 
 export default class Product extends Component {
   render() {
+    let {productTheme , title , supplementClass , supplement , detail} = this.props
+
     return (
-      <div className={`${this.props.productTheme} item`}>
+      <div className={`${productTheme} item`}>
         <div className='product-img-outer'><img alt='产品介绍图片' src={require(`../../assets/${this.props.productTheme}.png`)}></img></div>
         <div className='product-more-outer'>
-            <div className='product-title' children={`${this.props.title}`}></div>
-            <div className={`${this.props.supplementClass} product-supplement`} children={`${this.props.supplement}`}></div>
-            <div className='product-detail' children={`${this.props.detail}`}></div>
+            <div className='product-title' children={`${title}`}></div>
+            <div className={`${supplementClass} product-supplement`} children={`${supplement}`}></div>
+            <div className='product-detail' children={`${detail}`}></div>
         </div>
       </div>
     )
